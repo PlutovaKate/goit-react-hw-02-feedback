495aa5772070556a78a2e55bbd5842eb0255e517
PlutovaKate <114903556+PlutovaKate@users.noreply.github.com>
2023-06-27 22:32:00 +0300

    add render to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,76 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncreamentGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+  handleIncreamentNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  handleIncreamentBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>Please Leave feedback</h1>
+        <button type="button" onClick={this.handleIncreamentGood}>
+          Good
+        </button>
+        <button type="button" onClick={this.handleIncreamentNeutral}>
+          Neutral
+        </button>
+        <button type="button" onClick={this.handleIncreamentBad}>
+          Bad
+        </button>
+
+        {/* <ButtonItem
+          onIncreamentGood={this.handleIncreamentGood}
+          onIncreamentNeutral={this.handleIncreamentNeutral}
+          onIncreamentBad={this.handleIncreamentBad}
+        /> */}
+
+        <div>
+          <h1>Statistic</h1>
+          <h2>No feedback given</h2>
+          <ul>
+            <li>
+              Good:
+              <span>{this.state.good}</span>
+            </li>
+            <li>
+              Neutral:
+              <span>{this.state.neutral}</span>
+            </li>
+            <li>
+              Bad:
+              <span>{this.state.bad}</span>
+            </li>
+            <li>
+              Total:
+              <span>
+                {this.state.good + this.state.neutral + this.state.bad}
+              </span>
+            </li>
+            <li>
+              Positive feedback:
+              <span>0</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+// import ButtonItem from './ButtonItem';
+
+// class Feedback extends React.Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   handleIncreamentGood = () => {
+//     this.setState(prevState => ({
+//       good: prevState.good + 1,
+//     }));
+//   };
+//   handleIncreamentNeutral = () => {
+//     this.setState(prevState => ({
+//       neutral: prevState.neutral + 1,
+//     }));
+//   };
+//   handleIncreamentBad = () => {
+//     this.setState(prevState => ({
+//       bad: prevState.bad + 1,
+//     }));
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <h1>Please Leave feedback</h1>
+
+//         <ButtonItem
+//           onIncreamentGood={this.handleIncreamentGood}
+//           onIncreamentNeutral={this.handleIncreamentNeutral}
+//           onIncreamentBad={this.handleIncreamentBad}
+//         />
+
+//         <div>
+//           <h1>Statistic</h1>
+//           <h2>No feedback given</h2>
+//           <ul>
+//             <li>
+//               Good:
+//               <span>{this.state.good}</span>
+//             </li>
+//             <li>
+//               Neutral:
+//               <span>{this.state.neutral}</span>
+//             </li>
+//             <li>
+//               Bad:
+//               <span>{this.state.bad}</span>
+//             </li>
+//             <li>
+//               Total:
+//               <span>
+//                 {this.state.good + this.state.neutral + this.state.bad}
+//               </span>
+//             </li>
+//             <li>
+//               Positive feedback:
+//               <span>0</span>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Feedback;
